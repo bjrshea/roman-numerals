88dc5c89679e94c5bd39aef59b1de58f3322789d
Brendan Shea, Cam Peters and Josh Gearheart <josh@>
2018-08-16 10:04:48 -0700

    add functionality to recognize Roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
+var romanNumerals = [["I",1], ["V",5], ["X",10], ["L",50], ["C",100], ["D", 500], ["M",1000]];
+
 var romanize = function(number) {
-  var arabicInput = $("#user-input").val();
-  return arabicInput;
+  var arabicInput = parseInt($("#user-input").val());
+  for (i=0; i<7; i++)
+    if ((romanNumerals[i][1]) === arabicInput) {
+      var romanOutput = romanNumerals[i][0]
+      console.log(romanOutput);
+      return romanOutput
+    } else {
+      return "I'm sorry, Dave, I can't do that."
+    }
+  return romanOutput;
 };
 
 $(document).ready(function() {
   $("#form-input").submit(function(event){
       event.preventDefault();
-      romanize().text($("#result-insert"));
+      $("#result-insert").text(romanize());
   });
 });
