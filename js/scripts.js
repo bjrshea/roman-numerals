e999da11294dda653a681f603ae67e5cc2dcd77e
Brendan Shea, Cam Peters and Josh Gearheart <josh@>
2018-08-16 13:38:58 -0700

    recognize digits as multiples of ten

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,31 @@
-var romanNumerals = [["I",1], ["V",5], ["X",10], ["L",50], ["C",100], ["D", 500], ["M",1000]];
+var romanNumerals = [["I"], ["V"], ["X"], ["L"], ["C"], ["D"], ["M"]];
+var arabicNumerals = [[1], [5], [10], [50], [100], [500], [1000]];
 
-var romanize = function(number) {
-  var arabicInput = parseInt($("#user-input").val());
-  for (i=0; i<7; i++) {
-    if ((romanNumerals[i][1]) === arabicInput) {
-      var romanOutput = romanNumerals[i][0];
-      console.log(romanOutput);
-      return romanOutput;
-    }
-  }
+
+var numberConvert = function(string) {
+  var arabicInput = ($("#user-input").val());
+  var addZeroes = 4 - arabicInput.length;
+  for (var i=0; i<addZeroes; i++) {
+    arabicInput = "0" + arabicInput;
+    console.log(arabicInput);
+  };
+  var pos1000 = parseInt(arabicInput.charAt(0));
+  var pos100 = parseInt(arabicInput.charAt(1));
+  var pos10 = parseInt(arabicInput.charAt(2));
+  var pos1 = parseInt(arabicInput.charAt(3));
+  console.log(pos1000);
+  console.log(pos100);
+  console.log(pos10);
+  console.log(pos1);
 };
+
+
+
+
 
 $(document).ready(function() {
   $("#form-input").submit(function(event){
       event.preventDefault();
-      $("#result-insert").text(romanize());
+      $("#result-insert").text(numberConvert());
   });
 });
